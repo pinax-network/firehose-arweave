3a5e99816201d235de83ee09f80e78f6e0c1f9f7
Charles Morin <charles.mor@outlook.fr>
2024-01-10 15:02:55 -0500

    Implemented NewConsoleReader

--- cmd/firearweave/main.go
+++ cmd/firearweave/main.go
@@ -4,11 +4,47 @@ import (
 	pbarweave "github.com/pinax-network/firehose-arweave/types/pb/sf/arweave/type/v1"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	pbbstream "github.com/streamingfast/bstream/pb/sf/bstream/v1"
 	firecore "github.com/streamingfast/firehose-core"
 	fhCmd "github.com/streamingfast/firehose-core/cmd"
+	"github.com/streamingfast/firehose-core/node-manager/mindreader"
 	"github.com/streamingfast/logging"
 	"go.uber.org/zap"
 )
+
+type ConsoleReader struct {
+	lines        chan string
+	close        func()
+	blockEncoder firecore.BlockEncoder
+
+	done chan interface{}
+
+	logger *zap.Logger
+	tracer logging.Tracer
+}
+
+func (cr *ConsoleReader) Done() <-chan interface{} {
+	return cr.done
+}
+
+func (c *ConsoleReader) ReadBlock() (out *pbbstream.Block, err error) {
+	return nil, nil
+}
+
+func NewConsoleReader(lines chan string, blockEncoder firecore.BlockEncoder, logger *zap.Logger, tracer logging.Tracer) (mindreader.ConsolerReader, error) {
+	cr := &ConsoleReader{
+		lines:        lines,
+		close:        func() {},
+		blockEncoder: blockEncoder,
+
+		done: make(chan interface{}),
+
+		logger: logger,
+		tracer: tracer,
+	}
+
+	return cr, nil
+}
 
 func main() {
 	fhCmd.Main(Chain())
@@ -30,7 +66,8 @@ func Chain() *firecore.Chain[*pbarweave.Block] {
 
 		FirstStreamableBlock: 2,
 
-		BlockFactory: func() firecore.Block { return new(pbarweave.Block) },
+		BlockFactory:         func() firecore.Block { return new(pbarweave.Block) },
+		ConsoleReaderFactory: NewConsoleReader,
 
 		RegisterExtraStartFlags: func(flags *pflag.FlagSet) {
 			flags.String("reader-node-config-file", "", "Node configuration file, the file is copied inside the {data-dir}/reader/data folder Use {hostname} label to use short hostname in path")
